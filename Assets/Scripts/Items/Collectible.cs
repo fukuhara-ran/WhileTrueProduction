e5910408cc0c3dcca878a4fcf5e4d4c9ac343e5c
doddy-s <doddy.s@students.amikom.ac.id>
2024-06-19 10:52:45 +0700

    feat: add coin collection feature

--- Assets/Scripts/Items/Collectible.cs
+++ Assets/Scripts/Items/Collectible.cs
@@ -2,4 +2,11 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour {
     [SerializeField] protected Rigidbody2D rb;
+    protected ContactFilter2D contactFilter2D = new();
+    [SerializeField] protected Collider2D PlayerDetectorCollision;
+
+    protected void Erase() {
+        gameObject.SetActive(false);
+        Destroy(this);
+    }
 }
