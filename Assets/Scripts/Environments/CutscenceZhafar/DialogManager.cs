a108b73f5a6e8bec967598290a54fe3a58a45f78
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-06-23 08:11:50 +0700

    feat(Dialog Manager): introduce signals
    - using signal to trigger the initial conversation
    - the dialog can now auto move to next if typing is complete (delay 1s)

--- Assets/Scripts/Environments/CutscenceZhafar/DialogManager.cs
+++ Assets/Scripts/Environments/CutscenceZhafar/DialogManager.cs
@@ -6,9 +6,27 @@ public class DialogManager : MonoBehaviour
 {
     public TypingEffect typingEffect;
     public float typingSpeed = 0.05f;
+    public float delayBetweenDialogs = 1f;
     public Dialog currentDialog;
     private Conversation conversation;
+    public bool isInitial = false;
+    public static DialogManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void Start()
+    {
+        LoadDefaultConversation();
+    }
+    public void LoadDefaultConversation()
     {
         conversation = new Conversation();
         conversation.Enqueue("Justin", "For all the times that you rain on my parade");
@@ -22,26 +40,63 @@ public class DialogManager : MonoBehaviour
         conversation.Enqueue("Justin", "And I think you should be somethin' I don't wanna hold back");
         conversation.Enqueue("Girlfriend", "Maybe you should know that");
         conversation.Enqueue("Justin", "My mama don't like you and she likes everyone");
-        currentDialog = conversation.Dequeue();
+        conversation.Enqueue("Girlfriend", "And I never like to admit that I was wrong");
+        conversation.Enqueue("Justin", "And I've been so caught up in my job, didn't see what's going on");
+        conversation.Enqueue("Girlfriend", "But now I know, I'm better sleeping on my own");
+    }
+    public void LoadConversation(Conversation newConversation)
+    {
+        conversation = newConversation;
+    }
+
+    public void StartNewDialogueSequence()
+    {
+        typingEffect.SetText("");
+        typingEffect.SetName("");
+        isInitial = true;
     }
     public void Update()
     {
-        switch (typingEffect.isTyping)
+        if (conversation.IsEmpty() && !typingEffect.isTyping)
         {
-            case true:
-                if (Input.GetMouseButtonDown(0))
-                    typingEffect.SkipTyping();
-                break;
-            case false:
-                if (Input.GetMouseButtonDown(0))
-                {
-                    if (currentDialog != null)
-                    {
-                        typingEffect.StartTyping(currentDialog.Name, currentDialog.Text, typingSpeed);
-                        currentDialog = conversation.Dequeue();
-                    }
-                }
-                break;
+            Debug.Log("End of conversation");
+            return;
+        }
+
+        if (isInitial)
+        {
+            NextDialog();
+            isInitial = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (typingEffect.isTyping)
+            {
+                typingEffect.SkipTyping();
+            }
+            else
+            {
+                NextDialog();
+            }
+        }
+        if (!typingEffect.isTyping && !conversation.IsEmpty())
+        {
+            delayBetweenDialogs -= Time.deltaTime;
+            if (delayBetweenDialogs <= 0)
+            {
+                NextDialog();
+                delayBetweenDialogs = 1f;
+            }
+        }
+    }
+
+    private void NextDialog()
+    {
+        if (!conversation.IsEmpty())
+        {
+            currentDialog = conversation.Dequeue();
+            typingEffect.StartTyping(currentDialog.Name, currentDialog.Text, typingSpeed);
         }
     }
 }
