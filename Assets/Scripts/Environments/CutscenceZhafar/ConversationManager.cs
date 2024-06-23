b234469341423227bf030501a78e0fc1f497a119
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-06-23 22:16:20 +0700

    feat(dialog:) first encounter dialog complete however the timing is still need to be adjusted also refactor how dialogmanager works, it now uses state

--- Assets/Scripts/Environments/CutscenceZhafar/ConversationManager.cs
+++ Assets/Scripts/Environments/CutscenceZhafar/ConversationManager.cs
@@ -14,6 +14,9 @@ public class ConversationManager : MonoBehaviour
             case "bab1b":
                 Bab1b();
                 break;
+            case "bab1c":
+                Bab1c();
+                break;
             default:
                 break;
         }
@@ -35,6 +38,15 @@ public class ConversationManager : MonoBehaviour
         conversation.Enqueue("Vargoth", "I desire power, chaos, and the suffering of those who dare to oppose me.");
         conversation.Enqueue("Helena", "Whatever your aims are, we won't allow you to bring harm to these lands or its people.");
         conversation.Enqueue("Ash", "Allow? My dear, you speak as if you have a choice in the matter. Your determination is admirable, if woefully misplaced.");
+        dialogManager.LoadConversation(conversation);
+    }
+    public void Bab1c()
+    {
+        Conversation conversation = new Conversation();
+        conversation.Enqueue("Ash", "Helena, where are you?");
+        conversation.Enqueue("Vargoth's Echo", "Your cries are in vain, young hero. She is beyond your reach now.");
+        conversation.Enqueue("Ash", "I don't care what dark powers you wield, Vargoth. I will find you, and I will save her.");
+        conversation.Enqueue("Ash", "Hold on, Helena. No matter what trials lie ahead, I swear I'll find you. Our bond is stronger than any darkness.");
         dialogManager.LoadConversation(conversation);
     }
 }
